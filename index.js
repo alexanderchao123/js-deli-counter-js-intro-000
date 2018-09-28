d6905a57a3e0f97c4b1cb3a02709a880912479a5
alexanderchao123 <chao.alexander.123@gmail.com>
2018-09-28 01:46:12 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -11,3 +11,14 @@ function nowServing(line) {
     return `Currently serving ${first_person}.`
   }
 }
+
+function currentLine(line) {
+  var list = []
+  if (line.length === 0) {
+    return "The line is currently empty."
+  } else {
+    for (var i = 0, i < line.length; i++) {
+      list.push(`${i+1}. ${line[i]}`)
+    }
+  }
+}
