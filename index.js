f4163a73c8a364854a5ba53121dfdf58403f5839
alexanderchao123 <chao.alexander.123@gmail.com>
2018-09-28 01:47:47 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -17,7 +17,7 @@ function currentLine(line) {
   if (line.length === 0) {
     return "The line is currently empty."
   } else {
-    for (var i = 0, i < line.length; i++) {
+    for (var i = 0; i < line.length; i++) {
       list.push(`${i+1}. ${line[i]}`)
     }
   }
